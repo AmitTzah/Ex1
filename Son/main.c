a2962c6b59d090491a7076b9e0555a7f17c51178
Tomer Shimshi <tomershimshi@gmail.com>
2021-10-30 19:08:16 +0300

    added the methotds from the previos project chnged the main to only call methods

--- Son/main.c
+++ Son/main.c
@@ -11,44 +11,100 @@ int main(int argc, char *argv[])
 {
 	HANDLE message_file;
 	HANDLE Encrypted_message_file;
-	
+	const wchar_t TARGET_FILE[] = L"./Encrypted_message.txt";
 	OVERLAPPED offset_bytes;
 	LPDWORD num_of_bytes_written=NULL;
-	offset_bytes.Offset = 0;
+	offset_bytes.Offset = atoi(argv[2]);// accourding to instruction the first arg is the offset val
 	offset_bytes.OffsetHigh = 0;
+	char* Key = NULL;
+	char* EncryptedMessage = NULL;
+	char* Message = NULL;
 
+	//char* FileName = argv[1];// accourding to instruction the first arg is the file path
+	
+	//char* KeyFileName = argv[3];
+	//char* Message = NULL;
 	char test_[] = "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh";
 
-	message_file = CreateFileA(
-		argv[1],
-		GENERIC_READ,
-		0, 
-		NULL,
-		OPEN_EXISTING,
-		FILE_ATTRIBUTE_NORMAL,
-		NULL);
 
-	Encrypted_message_file = CreateFileA(
-		"Encrypted_message.txt",
-		GENERIC_WRITE, 
-		0,
-		NULL, 
-		OPEN_ALWAYS,
-		FILE_ATTRIBUTE_NORMAL,
-		NULL);
+
+	// just for test delete later
+	//---------#############TEST#############################---------
+	char FileName[] =  "./plaintext.txt";
+	int OffSet = 16;
+	char KeyFileName[] = ". / key.txt";
+	
+	EncryptMessage(&Key, &Message, NUM_OF_BYTES_TO_WRITE, &EncryptedMessage);
+	printf("EncryptedMessage = %s \n", EncryptedMessage);
+	WinWriteToFile(TARGET_FILE, &EncryptedMessage, NUM_OF_BYTES_TO_WRITE);
+
 	
 
-	if (message_file == INVALID_HANDLE_VALUE || Encrypted_message_file == INVALID_HANDLE_VALUE)
+
+	return 0;
+
+}
+
+void EncryptMessage(char** key, char** message, int length, char** encryptedMessage)
+{
+	int i = 0;
+	char* Encrypt = NULL;
+	char* Ptrkey = *key;
+	char* PtrMessge = *message;
+	Encrypt = malloc(sizeof(char) * length);
+	/*if (NULL == strcpy(*Encrypt, *message))
+	{
+		printf("failed to copy string \n");
+	}*/
+
+
+	for (i = 0; i < length; ++i) {
+		printf("message[i]= %c\n", *PtrMessge);
+		Encrypt[i] = *PtrMessge ^ *Ptrkey;
+
+		PtrMessge++;
+		Ptrkey++;
+	}
+	printf("Encrypt= %s\n", Encrypt);
+
+	*encryptedMessage = Encrypt;
+}
+
+/*
+* write to a file using the win API lib
+* based on the example from:
+* https://riptutorial.com/winapi/example/5736/create-a-file-and-write-to-it
+*/
+int WinWriteToFile(wchar_t  pathToFile[], char** stringToAppend, int MessegeLen)
+{
+	// Open a handle to the file
+	HANDLE hFile = CreateFile(
+		(LPCTSTR)(pathToFile),     // Filename
+		FILE_APPEND_DATA,          // Desired access
+		FILE_SHARE_READ,        // Share mode
+		NULL,                   // Security attributes
+		OPEN_ALWAYS,              // Creates a new file, only if it doesn't already exist and open it if the file exsits
+		FILE_ATTRIBUTE_NORMAL,  // Flags and attributes
+		NULL);                  // Template file handle
+
+	
+	if (hFile == INVALID_HANDLE_VALUE || hFile == INVALID_HANDLE_VALUE)
 	{
 		printf("Error opening file!");
 		exit(1);
 	}
 
-	WriteFile(Encrypted_message_file, test_, NUM_OF_BYTES_TO_WRITE, num_of_bytes_written , &offset_bytes);
 
-	printf("%ld", *num_of_bytes_written);
+	//printf("%ld", *num_of_bytes_written);
+	DWORD bytesWritten;
+	WriteFile(
+		hFile,            // Handle to the file
+		(DWORD)*stringToAppend,  // Buffer to write
+		MessegeLen,   // Buffer size
+		stringToAppend,    // Bytes written
+		NULL);         // Overlapped
 
-	return 0;
-
+	 // Close the handle once we don't need it.
+	CloseHandle(hFile);
 }
-// test push
+
---
+++ Son/main.h
@@ -0,0 +1,23 @@
+#pragma once
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include <stdlib.h>
+
+
+
+static const int ERR_CODE_NOT_ENOUGH_ARGUMENTS = -3;
+static const int ERR_CODE_TOO_MANY_ARGUMENTS = -2;
+static const int STATUS_CODE_FAILURE = -1;
+static const int STATUS_CODE_SUCCESS = 0;
+
+
+void split_str(char* str, char* FileName, char* OffSet, char* KeyFileName);
+char* getline(void);
+void SReadFile(char* path, char* WordArr, int length);
+void EncryptMessage(char** key, char** message, int length, char** encryptedMessage);
+int writeToFile(char pathToFile[], char** stringToAppend);
+int WinWriteToFile(LPCTSTR pathToFile, char** stringToAppend, int MessegeLen);
+int WinReadFromFile(LPCTSTR pathToFile, char* WordArr, int length, int OffSet);
