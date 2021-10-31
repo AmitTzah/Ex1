9dff86ec5aca6e55291d4a4947f3ede2bebdd7d9
AmitTzah <93420076+AmitTzah@users.noreply.github.com>
2021-10-31 18:30:39 +0200

    Son works correctly when  "Son.exe plaintext.txt 0 key.txt" is passed via the cmd, but when Father creates Son with the same command line, Son exits with error code.

--- Father/main.c
+++ Father/main.c
@@ -1,4 +1,7 @@
+
 #include "file_IO.h"
+#include "HardCodedData.h"
+#include "create_and_handle_processes.h"
 #include <stdio.h>
 #include < stddef.h >
 #include <stdlib.h>
@@ -6,6 +9,14 @@
 
 void main(int argc, char* argv[])
 {
+	int offset = 0;
+	char command_arguments_to_path_to_son[MAX_LENGTH_OF_PATH_TO_A_FILE];
+
+	strcpy_s(command_arguments_to_path_to_son, MAX_LENGTH_OF_PATH_TO_A_FILE, "Son.exe plaintext.txt 0 key.txt");
+
+
+
+	CreateProcessSimpleMain(command_arguments_to_path_to_son, TIMEOUT_IN_MILLISECONDS);
 	
 
 } 
--- Share/file_IO.c
+++ Share/file_IO.c
@@ -1,3 +1,5 @@
+
+
 #include "file_IO.h"
 #include <stdio.h>
 #include < stddef.h >
@@ -53,7 +55,7 @@ int WinReadFromFile(char* pathToFile, char* buffer_for_bytes_read, int num_of_by
 	if (hFile == INVALID_HANDLE_VALUE)
 	{
 		const int error = GetLastError();
-		printf("Error opening file %s! %d", pathToFile, error);
+		printf("Error opening file %s! %d\n", pathToFile, error);
 		exit(1);
 	}
 
@@ -65,7 +67,7 @@ int WinReadFromFile(char* pathToFile, char* buffer_for_bytes_read, int num_of_by
 	if (FALSE == res)
 	{
 		const int error = GetLastError();
-		printf("Error reading file! %d", error);
+		printf("Error reading file! %d\n", error);
 		exit(1);
 	}
 	CloseHandle(hFile);
@@ -114,7 +116,7 @@ https://riptutorial.com/winapi/example/5736/create-a-file-and-write-to-it
 	if (hFile == INVALID_HANDLE_VALUE)
 	{
 		const int error = GetLastError();
-		printf("WinWriteToFile Error opening file %s! %d", pathToFile, error);
+		printf("WinWriteToFile Error opening file %s! %d\n", pathToFile, error);
 		exit(1);
 	}
 
@@ -132,7 +134,7 @@ https://riptutorial.com/winapi/example/5736/create-a-file-and-write-to-it
 	if (FALSE == res)
 	{
 		const int error = GetLastError();
-		printf("Error reading file! %d", error);
+		printf("Error reading file! %d\n", error);
 		exit(1);
 	}
 
@@ -140,4 +142,58 @@ https://riptutorial.com/winapi/example/5736/create-a-file-and-write-to-it
 	CloseHandle(hFile);
 
 	return bytesWritten;
+}
+
+	
+
+	
+
+	// Based on:
+	/*
+	https://www.geeksforgeeks.org/c-program-to-count-the-number-of-characters-in-a-file/
+	https://www.educative.io/edpresso/what-is-fopens-in-c
+	*/
+
+
+	/// <summary>
+	/// 
+	/// </returns number of characters in file>
+	/// <param name="pathToFile"></a path to file location>
+	/// <returns></number of characters in file>
+	
+	int return_numbers_of_chars_in_file(char* pathToFile) {
+
+
+		FILE* fp;
+
+		// Declaring a variable of type errno_t to store the return value of fopen_s
+		errno_t error_code;
+	
+		//Opening file in r mode
+		error_code = fopen_s(&fp, pathToFile , "r");
+
+		if (error_code != 0) {
+			printf("Error! Failed to open file in r mode!\n");
+			exit(1);
+		}
+		else{
+		// Character counter (result)
+		int count = 0;
+
+		// To store a character read from file
+		char c=0;
+
+		// Extract characters from file
+		// and store in character c
+		for (c = getc(fp); c != EOF; c = getc(fp))
+
+			// Increment count for this character
+			count = count + 1;
+
+		// Close the file
+		fclose(fp);
+
+		return count;
+		}
+
 }
